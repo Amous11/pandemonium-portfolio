b41c34653f0ff483a8162a0dfb16121add85a01e
Youssef Amous <youssefamous11@gmail.com>
2024-05-25 15:10:47 +0100

    netlify test

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base : "https://amous11.github.io/pandemonium-portfolio/",
   plugins: [react()],
 })
