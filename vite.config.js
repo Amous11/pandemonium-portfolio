a9b83dd29f9b7016b0fc0ee2fd591232c11a25c4
Youssef Amous <youssefamous11@gmail.com>
2024-05-29 08:08:43 +0100

    revert vite config

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  build:{
-    outDir: '~/public_html',
-    emptyOutDir: true
-  }
+  plugins: [react()]
+  
 })
